3e2632b6e12f461db7ecb9ede3ffe56c9c14449f
a_kiritani <a.kiritani@skillengine.jp>
2023-01-24 23:49:02 +0900

    修正

--- src/containers/Foods.jsx
+++ src/containers/Foods.jsx
@@ -94,7 +94,7 @@ export const Foods = ({
     <Fragment>
       <HeaderWrapper>
         <Link to="/restaurants">
-          Monzen Search5
+          Monzen Search
           {/* <MainLogoImage src={MainLogo} alt="main logo" /> */}
         </Link>
         <BagIconWrapper>
